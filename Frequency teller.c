37610bd643535d03cb3cba789732532e0e2b7dfc
Slayer <68937752+abcd454@users.noreply.github.com>
2024-12-07 22:44:40 +0530

    Create Frequency teller.c
    Frequency of Digits in a Number.

---
+++ Frequency teller.c
@@ -0,0 +1,18 @@
+def digit_frequency(num):
+    if num < 0:
+        num = -num  # Handle negative numbers
+    freq = [0] * 10
+
+    while num > 0:
+        digit = num % 10
+        freq[digit] += 1
+        num //= 10
+
+    return freq
+
+# Test cases
+assert digit_frequency(1122334455) == [0, 2, 2, 2, 2, 2, 0, 0, 0, 0]
+assert digit_frequency(9876543210) == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+assert digit_frequency(0) == [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+assert digit_frequency(-12321) == [0, 2, 2, 1, 0, 0, 0, 0, 0, 0]
+print("All tests passed!")
